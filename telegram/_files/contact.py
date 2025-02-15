b142f3c0f3fb5e158c3b82e2f7ece4202ca6c44a
Relaxx422 <ismailsandim@gmail.com>
2025-02-15 16:20:48 +0300

    Update contact.py

--- telegram/_files/contact.py
+++ telegram/_files/contact.py
@@ -1,71 +1,43 @@
-#!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2025
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains an object that represents a Telegram Contact."""
-from typing import Optional
-
-from telegram._telegramobject import TelegramObject
-from telegram._utils.types import JSONDict
-
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 class Contact(TelegramObject):
-    """This object represents a phone contact.
+    """Represents a phone contact."""
+    
+    def __init__(self, phone_number, first_name, last_name=None, user_id=None, vcard=None):
+        super().__init__()
+        self.phone_number = phone_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_id = user_id
+        self.vcard = vcard
 
-    Objects of this class are comparable in terms of equality. Two objects of this class are
-    considered equal, if their :attr:`phone_number` is equal.
+def handle_contact(update: Update, context: CallbackContext):
+    contact = update.message.contact
+    contact_obj = Contact(
+        phone_number=contact.phone_number,
+        first_name=contact.first_name,
+        last_name=contact.last_name,
+        user_id=contact.user_id,
+        vcard=contact.vcard
+    )
+    update.message.reply_text(f"Kişi alındı: {contact_obj.first_name} {contact_obj.last_name or ''}")
 
-    Args:
-        phone_number (:obj:`str`): Contact's phone number.
-        first_name (:obj:`str`): Contact's first name.
-        last_name (:obj:`str`, optional): Contact's last name.
-        user_id (:obj:`int`, optional): Contact's user identifier in Telegram.
-        vcard (:obj:`str`, optional): Additional data about the contact in the form of a vCard.
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Merhaba! Bir kişi gönderin ve bilgilerini alacağım.')
 
-    Attributes:
-        phone_number (:obj:`str`): Contact's phone number.
-        first_name (:obj:`str`): Contact's first name.
-        last_name (:obj:`str`): Optional. Contact's last name.
-        user_id (:obj:`int`): Optional. Contact's user identifier in Telegram.
-        vcard (:obj:`str`): Optional. Additional data about the contact in the form of a vCard.
+def main():
+    # Buraya kendi token'ınızı ekleyin
+    TOKEN = "7784466023:AAHV5exN22Ply1jeskJ9ffZ7b3WsHWIiCPE"
+    
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-    """
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.contact, handle_contact))
 
-    __slots__ = ("first_name", "last_name", "phone_number", "user_id", "vcard")
+    updater.start_polling()
+    updater.idle()
 
-    def __init__(
-        self,
-        phone_number: str,
-        first_name: str,
-        last_name: Optional[str] = None,
-        user_id: Optional[int] = None,
-        vcard: Optional[str] = None,
-        *,
-        api_kwargs: Optional[JSONDict] = None,
-    ):
-        super().__init__(api_kwargs=api_kwargs)
-        # Required
-        self.phone_number: str = str(phone_number)
-        self.first_name: str = first_name
-        # Optionals
-        self.last_name: Optional[str] = last_name
-        self.user_id: Optional[int] = user_id
-        self.vcard: Optional[str] = vcard
-
-        self._id_attrs = (self.phone_number,)
-
-        self._freeze()
+if __name__ == '__main__':
+    main()
